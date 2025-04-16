3d01fa0f6a897e049e7aa443e6b603fa0dcd38dc
Wildan Arif Pradana <wildanarif268@gmail.com>
2025-04-16 13:38:12 +0700

    admin template and migration

---
+++ database/migrations/2025_04_16_040130_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid');
+            $table->string('name');
+            $table->string('email');
+            $table->string('number');
+            $table->text('address');
+            $table->integer('transaction_total');
+            $table->string('transaction_status');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('', [DashboardController::class, 'index']);
+
+Auth::routes(['register' => false]);
